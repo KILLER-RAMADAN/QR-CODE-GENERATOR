da7f91a674f6f5eda0c3c7d4bc25ea3c9f97e599
AHMED RAMADAN <ahmedramadan5452332@gmail.com>
2023-07-06 15:54:22 +0300

    QR-CODE.py

--- QR-CODE-GENERATOR/QR-CODE.py
+++ QR-CODE-GENERATOR/QR-CODE.py
@@ -3,7 +3,7 @@ from pyqrcode import create
 import tkinter as tk
 from tkinter import * 
 from tkinter import messagebox
-from PIL import Image 
+from PIL import Image ,ImageTk
 import PIL 
 import os
 from tkinter import filedialog
@@ -34,64 +34,80 @@ def simple_qr_code():
  window2 = tk.Tk()
  window2.resizable(0,0)
  window2.geometry("800x500+400+100")   
- window2.title("AOU_QRCODE_GENERATOR")  
+ window2.title("Qr-Code")  
  window2.iconbitmap('app-photos/qr.ico')
  window2.attributes('-topmost', True)
+ window2.configure(bg="#121212")
  #main compononts....
  user_link1=tk.Entry(window2,font=("Arial",15,"bold"),bg='white',width=45)
  user_link1.place(x=140,y=60)
+ insert_link=user_link1.clipboard_get()
+ user_link1.insert(0,insert_link)
  user_link2=tk.Entry(window2,font=("Arial",15,"bold"),bg='white',width=45)
  user_link2.place(x=140,y=140)
  user_location=tk.Entry(window2,font=("Arial",15,"bold"),bg='white',width=45)
  user_location.place(x=140,y=100)
  user_location.configure(state=DISABLED)
- button1=tk.Button(window2,font=("Arial",10,"bold"),height=1,width=18,text='Make Qr_Code',fg="white",background="#0F115B",bd=0,activebackground="#0F115B",command=lambda:my_generate_2())
+ button1=tk.Button(window2,font=("Arial",10,"bold"),height=1,width=18,text='Make Qr_Code',relief="groove",fg="#8E44AD",background="#121212",activeforeground="#8E44AD",activebackground="#121212",command=lambda:my_generate_2())
  button1.place(x=645,y=60)
- button2=tk.Button(window2,font=("Arial",10,"bold"),height=1,width=18,text='Clear',fg="white",background="red",activebackground="red",bd=0,command=lambda:clear())
+ button2=tk.Button(window2,font=("Arial",10,"bold"),height=1,width=18,text='Clear',fg="#8E44AD",relief="groove",background="#121212",activeforeground="#8E44AD",activebackground="#121212",command=lambda:clear())
  button2.place(x=645,y=140)
- button3=tk.Button(window2,font=("Arial",10,"bold"),height=1,width=18,text='location',fg="white",background="green",activebackground="green",bd=0,command=lambda:file_location())
+ button3=tk.Button(window2,font=("Arial",10,"bold"),height=1,width=18,text='location',fg="#8E44AD",relief="groove",background="#121212",activeforeground="#8E44AD",activebackground="#121212",command=lambda:file_location())
  button3.place(x=645,y=100)
- Label0=tk.Label(window2,text='LINK HERE:',fg="black",font=("Bakbak One",15,"bold"))
+ Label0=tk.Label(window2,text='Input Link:',fg="white",bg="#121212",font=("Bakbak One",15,"bold"))
  Label0.place(x=0,y=60)
- Label1=tk.Label(window2,text='QR-NAME:',fg="black",font=("Bakbak One",15,"bold"))
+ Label1=tk.Label(window2,text='Qr-Name:',fg="white",bg="#121212",font=("Bakbak One",15,"bold"))
  Label1.place(x=0,y=140)
- Label2=tk.Label(window2,text='LOCATION:',fg="black",font=("Bakbak One",15,"bold"))
+ Label2=tk.Label(window2,text='Qr-Location:',fg="white",bg="#121212",font=("Bakbak One",15,"bold"))
  Label2.place(x=0,y=100)
- Label3=tk.Label(window2)
- Label3.place(x=280,y=170)
- Frame(window2,width=800,height=50,bg="black").place(x=0,y=450)
- Frame(window2,width=800,height=50,bg="black").place(x=0,y=0)
- Label(window2,text="QR-CODE-GENERATOR",fg="white",bg="black",font=("Amiri",20,"bold")).place(x=245,y=10)
+ Label3=tk.Label(window2,bg="#121212")
+ Label3.place(x=295,y=190)
+ 
+ Label4=tk.Label(window2,text="My Github Qr-Code",fg="#8E44AD",font=("Bakbak One",15,"bold"),bg="#121212")
+ Label4.place(x=302,y=400)
+
+ my_git_locatoin="main-qrcode\\git-qr.png"
+ my_git_img=ImageTk.PhotoImage(Image.open(my_git_locatoin).resize((200,200)))
+ Label3.configure(image=my_git_img,bg="#121212")
+ 
+ 
+ 
+ Label(window2,text="QR-CODE-GENERATOR",fg="#8E44AD",bg="#121212",font=("Amiri",20,"bold")).place(x=245,y=10)
  Frame(window2,width=800,height=50,bg="#0F115B").place(x=0,y=700)
- Label(window2,text="Copyright © 'RAMADAN' 2023 All Rights Reserved.",fg="white",bg="black",font=("Bakbak One",10,"bold")).place(x=270,y=450)
+ Label(window2,text="Copyright © 'AHMED RAMADAN' 2023 All Rights Reserved.",fg="#8E44AD",bg="#121212",font=("Bakbak One",10,"bold")).place(x=230,y=450)
 #_______________________________________
  def my_generate_2():
     global file_locate
     global my_img
-    link="https:"
-    if link not in user_link1.get():
-      messagebox.showinfo("ENTER LINK","PLEASE INPUT CORRECT LINK...",parent=window2)
+    global my_qr
+    if user_link1.get()=="":
+      messagebox.showinfo("ENTER LINK","Input Link...",parent=window2)
     elif user_link2.get()=="":
-      messagebox.showinfo("ENTER NAME","PLEASE INPUT QR-CODE NAME...",parent=window2)
+      messagebox.showinfo("ENTER NAME","Input Qr-Code Name...",parent=window2)
     elif user_location.get()=="":
-      messagebox.showinfo("ENTER NAME","PLEASE INPUT YOUR LOCATION TO SAVE...",parent=window2)
+      messagebox.showinfo("ENTER NAME","Input Location to Save Qr-Code...",parent=window2)
     else:
      try:
       my_qr = pyqrcode.create(f"{user_link1.get()}") 
-      my_qr.png('main-qrcode/test.png', scale=5, module_color=[0, 0, 0, 128], background=[0xff, 0xcc, 0xcc])
+      main_image_location="main-qrcode/test.png"
+      my_qr.png('main-qrcode/test.png', scale=5, module_color=[0, 0, 0, 255])
       my_qr = my_qr.xbm(scale=5)
-      my_img=tk.BitmapImage(data=my_qr)
+      my_img=ImageTk.PhotoImage(Image.open(main_image_location).resize((200,200)))
       Label3.config(image=my_img)
       img = Image.open(f"main-qrcode\\test.png") 
-      img = img .save(f"{file_locate}//{user_link2.get().upper()}-QR-CODE.png")
-      messagebox.showinfo("QR-CODE","QR-CODE CREATED SUCCESSFULLY..",parent=window2)
+      img = img .save(f"{file_locate}//{user_link2.get()}-Qr-Code.png")
+      Label4.configure(text="       Scan Now    ")
+      messagebox.showinfo("Qr-Code","Qr-Code Created Successfully.....",parent=window2)
      except:
-       messagebox.showerror("ERROR","1)WRONG FILE LOCATION")
+       return ""
  def clear():
-   clear1=user_link1.delete(0,100) 
-   clear2=user_link2.delete(0,100)
-   clear3=user_location.delete(0,100)
-   Label3.config(text="QR-CODE-AHMED RAMADAN.",font=("Bakbak One",25,"bold"))
+   clear1=user_link1.delete(0,1000) 
+   clear2=user_link2.delete(0,1000)
+   clear3=user_location.delete(0,1000)
+   my_git_locatoin="main-qrcode\\git-qr.png"
+   my_git_img=ImageTk.PhotoImage(Image.open(my_git_locatoin).resize((200,200)))
+   Label3.configure(image=my_git_img,bg="#121212")
+   Label4.configure(text="Clear Done".center(22," "))
    return clear1 ,clear2 ,clear3
  window2.mainloop()
 simple_qr_code()
